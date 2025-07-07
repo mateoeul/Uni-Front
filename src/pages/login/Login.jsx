e389a67a653c9af142584544db0e03fc959601a2
Android Studio <devandroid@ort>
2025-07-07 10:18:54 -0300

    login casi

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,7 +2,8 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./login.css"
-import Logo from "../../assets/images/uni.png"
+import Logo from "../../assets/images/uni.jpg";
+import Img from "../../assets/images/google.png"
 const Login = () => {
   const [form, setForm] = useState({
     email: "",
@@ -20,62 +21,64 @@ const Login = () => {
 
   return (
     <div className="login-container">
-      {/* Columna izquierda */}
-      <div className="login-left">
-        <img src={Logo} alt="Logo Uni" className="logo-img" />
-        <h1 className="login-title-left">
-          El comienzo de tu <br /> 
-          <span>futuro académico</span>
-        </h1>
-        <img
-          src="/illustration.png"
-          alt="Ilustración"
-          className="login-illustration"
-        />
-      </div>
+      <div className="login-wrapper">
+        {/* Columna izquierda */}
+        <div className="login-left">
+          <img src="src/assets/images/uni.jpg" alt="Logo Uni" className="logo-img" />
+          <h1 className="login-title-left">
+            El comienzo de tu <br />
+            <span>futuro académico</span>
+          </h1>
+          <img
+            src="/illustration.png"
+            alt="Ilustración"
+            className="login-illustration"
+          />
+        </div>
 
-      {/* Columna derecha */}
-      <div className="login-right">
-        <div className="login-box-final">
-          <h2 className="login-title">Iniciar sesión en Uni</h2>
+        {/* Columna derecha */}
+        <div className="login-right">
+          <div className="login-box-final">
+            <h2 className="login-title">Iniciar sesión en Uni</h2>
 
-          <form onSubmit={handleSubmit} className="login-form">
-            <input
-              type="email"
-              name="email"
-              placeholder="Correo electrónico"
-              value={form.email}
-              onChange={handleChange}
-              className="login-input-final"
-              required
-            />
-            <input
-              type="password"
-              name="password"
-              placeholder="Contraseña"
-              value={form.password}
-              onChange={handleChange}
-              className="login-input-final"
-              required
-            />
-            <button type="submit" className="login-button-final">
-              Iniciar sesión
+            <form onSubmit={handleSubmit} className="login-form">
+              <input
+                type="email"
+                name="email"
+                placeholder="Correo electrónico"
+                value={form.email}
+                onChange={handleChange}
+                className="login-input-final"
+                required
+              />
+              <input
+                type="password"
+                name="password"
+                placeholder="Contraseña"
+                value={form.password}
+                onChange={handleChange}
+                className="login-input-final"
+                required
+              />
+              <button type="submit" className="login-button-final">
+                Iniciar sesión
+              </button>
+            </form>
+
+            <button className="login-google-final">
+              <img
+                src="src/assets/images/google.png"
+                alt="Google"
+                className="google-icon"
+              />
+              Continuar con google
             </button>
-          </form>
 
-          <button className="login-google-final">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-              alt="Google"
-              className="google-icon"
-            />
-            Continuar con google
-          </button>
-
-          <div className="login-links-final">
-            <Link to="#">Olvidé mi contraseña</Link>
-            <br />
-            <Link to="/register">Crear una cuenta de Uni</Link>
+            <div className="login-links-final">
+              <Link to="#">Olvidé mi contraseña</Link>
+              <br />
+              <Link to="/register">Crear una cuenta de Uni</Link>
+            </div>
           </div>
         </div>
       </div>
